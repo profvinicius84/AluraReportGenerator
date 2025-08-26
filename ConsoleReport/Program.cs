a86d4e83dab1485bbc8efb4de0a7625e296c8393
alura <rodsordi@gmail.com>
2025-08-26 19:27:15 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleReport/Program.cs
@@ -0,0 +1,29 @@
+﻿using ReportGenerator;
+
+List<Dictionary<string, string>> data = new()
+{
+    new Dictionary<string, string>
+    {
+        { "Nome", "Alice" },
+        { "Idade", "30" },
+        { "Cidade", "São Paulo" }
+    },
+    new Dictionary<string, string>
+    {
+        { "Nome", "Roberto" },
+        { "Idade", "25" },
+        { "Cidade", "Salvador" }
+    },
+    new Dictionary<string, string>
+    {
+        { "Nome", "Carlos" },
+        { "Idade", "35" },
+        { "Cidade", "Rio de Janeiro" }
+    }
+};
+
+ReporterGenerator reporterGenerator = new ReporterGenerator(data);
+
+string caminhoArquivo = reporterGenerator.GenerateReport();
+
+Console.WriteLine("O arquivo foi salvo em: " + caminhoArquivo);
---
+++ ReportGenerator/ReporterGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportGenerator
+{
+    public class ReporterGenerator
+    {
+        public List<Dictionary<string, string>> Input { get; set; }
+
+        public ReporterGenerator(List<Dictionary<string, string>> input) 
+        { 
+            Input = input;
+        }
+
+        public string GenerateReport()
+        {
+            if (Input == null || Input.Count == 0)
+                throw new Exception("Input de dados invalido");
+
+            StringBuilder report = new StringBuilder();
+
+            string header = string.Join(';', Input.First().Keys);
+
+            report.AppendLine(header);
+
+            foreach (var record in Input)
+            {
+                string line = string.Join(";", record.Values);
+                report.AppendLine(line);
+            }
+
+            File.WriteAllText("report.csv", report.ToString());
+
+            return Path.GetFullPath("report.csv");
+        }
+    }
+}
